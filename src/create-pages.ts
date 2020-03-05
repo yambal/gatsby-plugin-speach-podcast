51f1f89f6be1b1f46cfbbf8927949e15a5a4b3d4
June YAMAMOTO <june@yambal.net>
2020-03-05 23:53:33 +0900

    0.3.0

--- src/create-pages.ts
+++ src/create-pages.ts
@@ -4,6 +4,7 @@ import { cacheToPablic, podcastCashSet, checkCache, iPodcastCacheCheckResponse }
 import { getChannelTitle, getChannelDescription, getGoogleProjectId, getGoogleKeyFileName } from './libs/option-parser'
 import { mdToSsml } from 'md-to-ssl'
 import { iPodcastEdge, iPluginOption } from './libs/interfaces'
+import { mdToMp3 } from 'md-to-google-ssml'
 
 export interface iPodcastBuild {
   edge: any
@@ -19,15 +20,13 @@ export interface iPodcastBuild {
 
 const podcastBuildMp3 = (
   checkCacheResponse: iPodcastCacheCheckResponse,
-  ssml: string,
+  markdown: string,
   projectId?: string | null,
   keyFilename?: string | null
 ) => {
-
   return new Promise((resolve: (resolve: iPodcastCacheCheckResponse) => void) => {
     if (!checkCacheResponse.hasCashe || checkCacheResponse.isOld) {
       console.log('podcast: make mp3')
-
       console.log('projectId', projectId)
       console.log('keyFilename', keyFilename)
 
@@ -35,6 +34,15 @@ const podcastBuildMp3 = (
         throw new Error('error: projectId, keyFilename')
       }
 
+      mdToMp3(markdown, { projectId, keyFileName: keyFilename })
+      .then(
+        audioData => {
+          console.log('podcast: make mp3 success')
+          checkCacheResponse.audioData = audioData
+          resolve(checkCacheResponse)
+        }
+      )
+      /*
       mp3(ssml, projectId, keyFilename)
       .then(
         audioData => {
@@ -43,6 +51,7 @@ const podcastBuildMp3 = (
           resolve(checkCacheResponse)
         }
       )
+      */
 
     } else {
       console.log('podcast: make mp3 skip')
@@ -90,12 +99,11 @@ const podcastEdgeToFile = (edge: iPodcastEdge, options: iPluginOption):Promise<i
         const channelTitle = getChannelTitle(channel, options)
         const channelDescription = getChannelDescription(channel, options)
 
-        // const ssml = HtmlToSSML(channelTitle, channelDescription, title, html)
+        return podcastBuildMp3(checkCacheResponse, rawMarkdownBody, getGoogleProjectId(options), getGoogleKeyFileName(options))
+        /*
         const ssml = mdToSsml(rawMarkdownBody, title, channelDescription, { google: true })
-        console.log(title, `length:${ssml.length}`)
-        // console.log(ssml)
-
         return podcastBuildMp3(checkCacheResponse, ssml, getGoogleProjectId(options), getGoogleKeyFileName(options))
+        */
       }
     )
     .then(
